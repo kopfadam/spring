0160807d0350490e275f952402111d2db0011019
Adam Kopf <adamkopf0@gmail.com>
2020-10-16 12:29:45 +0300

    initial commit for petclinic project

---
+++ pet-clinic-rest-clone/src/main/java/com/aga/boot/PetClinicRestCloneApplication.java
@@ -0,0 +1,13 @@
+package com.aga.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicRestCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicRestCloneApplication.class, args);
+	}
+
+}
---
+++ pet-clinic-rest-clone/src/test/java/com/aga/boot/PetClinicRestCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aga.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicRestCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
