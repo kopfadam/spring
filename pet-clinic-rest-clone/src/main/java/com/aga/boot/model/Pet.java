03580060689e945c0f742d13b54c20cae3e79bb7
Adam Kopf <adamkopf0@gmail.com>
2020-10-17 17:54:32 +0300

    created pet type and pet entities

--- pet-clinic-rest-clone/src/main/java/com/aga/boot/model/Pet.java
+++ pet-clinic-rest-clone/src/main/java/com/aga/boot/model/Pet.java
@@ -1,4 +1,58 @@
 package com.aga.boot.model;
 
-public class Pet {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "pets")
+@Getter
+@Setter
+public class Pet extends NamedEntity{
+
+    @Column(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date birthDate;
+
+    @ManyToOne
+    @Column(name = "type_id")
+    private PetType type;
+
+    @ManyToOne
+    @Column(name = "owner_id")
+    private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.EAGER)
+    private Set<Visit> visits;
+
+    @JsonIgnore
+    protected Set<Visit> getVisitsInternal(){
+        if (this.visits == null)
+            this.visits = new HashSet<>();
+
+        return this.visits;
+    }
+
+    protected void setVisitsInternal(Set<Visit> visits){
+        this.visits = visits;
+    }
+
+    public List<Visit> getVisits(){
+        List<Visit> sortedVisits = new ArrayList<>(getVisitsInternal());
+        PropertyComparator.sort(sortedVisits, new MutableSortDefinition("date", false, false));
+        return Collections.unmodifiableList(sortedVisits);
+    }
+
+    public void addVisit(Visit visit){
+        visit.setPet(this);
+        getVisitsInternal().add(visit);
+
+    }
 }
---
+++ pet-clinic-rest-clone/src/main/java/com/aga/boot/model/PetType.java
@@ -0,0 +1,10 @@
+package com.aga.boot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
+public class PetType extends NamedEntity{
+
+}
