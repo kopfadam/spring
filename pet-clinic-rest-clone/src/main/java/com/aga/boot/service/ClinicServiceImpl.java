61d5cdee9fa1707d8a3252262400b911750d3289
Adam Kopf <adamkopf0@gmail.com>
2020-10-27 12:48:59 +0200

    add clinic service implementation methods

--- pet-clinic-rest-clone/src/main/java/com/aga/boot/service/ClinicServiceImpl.java
+++ pet-clinic-rest-clone/src/main/java/com/aga/boot/service/ClinicServiceImpl.java
@@ -1,150 +1,221 @@
 package com.aga.boot.service;
 
 import com.aga.boot.model.*;
+import com.aga.boot.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.orm.ObjectRetrievalFailureException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 
 @Service
-public class ClinicServiceImpl implements ClinicService{
+@Transactional
+public class ClinicServiceImpl implements ClinicService {
+
+    private PetRepository petRepository;
+    private VetRepository vetRepository;
+    private OwnerRepository ownerRepository;
+    private VisitRepository visitRepository;
+    private SpecialtyRepository specialtyRepository;
+    private PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public ClinicServiceImpl(
+            PetRepository petRepository,
+            VetRepository vetRepository,
+            OwnerRepository ownerRepository,
+            VisitRepository visitRepository,
+            SpecialtyRepository specialtyRepository,
+            PetTypeRepository petTypeRepository
+    ) {
+        this.petRepository = petRepository;
+        this.vetRepository = vetRepository;
+        this.ownerRepository = ownerRepository;
+        this.visitRepository = visitRepository;
+        this.specialtyRepository = specialtyRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
     @Override
     public Pet findPetById(int id) throws DataAccessException {
-        return null;
+        Pet pet = null;
+        try {
+            pet = petRepository.findById(id);
+        } catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+            return null;
+        }
+        return pet;
     }
 
     @Override
     public Collection<Pet> findAllPets() throws DataAccessException {
-        return null;
+        return petRepository.findAll();
     }
 
     @Override
     public void savePet(Pet pet) throws DataAccessException {
-
+        petRepository.save(pet);
     }
 
     @Override
     public void deletePet(Pet pet) throws DataAccessException {
-
+        petRepository.delete(pet);
     }
 
     @Override
     public Visit findVisitById(int visitId) throws DataAccessException {
-        return null;
+        Visit visit = null;
+        try {
+            visit = visitRepository.findById(visitId);
+        } catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+            return null;
+        }
+        return visit;
     }
 
     @Override
     public Collection<Visit> findVisitsByPetId(int petId) {
-        return null;
+
+        return visitRepository.findByPetId(petId);
+
     }
 
     @Override
     public Collection<Visit> findAllVisits() throws DataAccessException {
-        return null;
+        return visitRepository.findAll();
     }
 
     @Override
     public void saveVisit(Visit visit) throws DataAccessException {
-
+        visitRepository.save(visit);
     }
 
     @Override
     public void deleteVisit(Visit visit) throws DataAccessException {
-
+        visitRepository.delete(visit);
     }
 
     @Override
     public Vet findVetById(int id) throws DataAccessException {
-        return null;
+        Vet vet = null;
+        try {
+            vet = vetRepository.findById(id);
+        } catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+            return null;
+        }
+        return vet;
     }
 
     @Override
+    @Cacheable(value = "vets")
     public Collection<Vet> findVets() throws DataAccessException {
-        return null;
+        return vetRepository.findAll();
     }
 
     @Override
     public Collection<Vet> findAllVets() throws DataAccessException {
-        return null;
+        return vetRepository.findAll();
     }
 
     @Override
     public void saveVet(Vet vet) throws DataAccessException {
-
+        vetRepository.save(vet);
     }
 
     @Override
     public void deleteVet(Vet vet) throws DataAccessException {
-
+        vetRepository.delete(vet);
     }
 
     @Override
     public Owner findOwnerById(int id) throws DataAccessException {
-        return null;
+        Owner owner = null;
+        try {
+            owner = ownerRepository.findById(id);
+        } catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+            return null;
+        }
+        return owner;
     }
 
     @Override
     public Collection<Owner> findOwnerByLastName(String lastName) throws DataAccessException {
-        return null;
+        return ownerRepository.findByLastName(lastName);
     }
 
     @Override
     public Collection<Owner> findAllOwners() throws DataAccessException {
-        return null;
+        return ownerRepository.findAll();
     }
 
     @Override
     public void saveOwner(Owner owner) throws DataAccessException {
-
+        ownerRepository.save(owner);
     }
 
     @Override
     public void deleteOwner(Owner owner) throws DataAccessException {
-
+        ownerRepository.delete(owner);
     }
 
     @Override
     public PetType findPetTypeById(int petTypeId) {
-        return null;
+        PetType petType = null;
+        try {
+            petType = petTypeRepository.findById(petTypeId);
+        } catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+            return null;
+        }
+        return petType;
     }
 
     @Override
     public Collection<PetType> findAllPetTypes() throws DataAccessException {
-        return null;
+        return petTypeRepository.findAll();
     }
 
     @Override
     public Collection<PetType> findPetTypes() throws DataAccessException {
-        return null;
+        return petRepository.findPetTypes();
     }
 
     @Override
     public void savePetType(PetType petType) throws DataAccessException {
-
+        petTypeRepository.save(petType);
     }
 
     @Override
     public void deletePetType(PetType petType) throws DataAccessException {
-
+        petTypeRepository.delete(petType);
     }
 
     @Override
     public Specialty findSpecialtyById(int specialtyId) {
-        return null;
+        Specialty specialty = null;
+        try {
+            specialty = specialtyRepository.findById(specialtyId);
+        } catch (ObjectRetrievalFailureException | EmptyResultDataAccessException e) {
+            return null;
+        }
+        return specialty;
     }
 
     @Override
     public Collection<Specialty> findAllSpecialties() throws DataAccessException {
-        return null;
+        return specialtyRepository.findAll();
     }
 
     @Override
     public void saveSpecialty(Specialty specialty) throws DataAccessException {
-
+        specialtyRepository.save(specialty);
     }
 
     @Override
     public void deleteSpecialty(Specialty specialty) throws DataAccessException {
-
+        specialtyRepository.delete(specialty);
     }
 }
