76d5c9138fc89c7007e654ca083a47d03454d266
Adam Kopf <adamkopf0@gmail.com>
2020-10-31 12:12:37 +0200

    modify -- redefine relation (from bidirectional to unidirectional) between pet and owner entities in order to manage queries easily

--- pet-clinic-rest-clone/src/main/java/com/aga/boot/model/Owner.java
+++ pet-clinic-rest-clone/src/main/java/com/aga/boot/model/Owner.java
@@ -1,18 +1,12 @@
 package com.aga.boot.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.support.MutableSortDefinition;
-import org.springframework.beans.support.PropertyComparator;
-import org.springframework.core.style.ToStringCreator;
 
 import javax.persistence.*;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.util.*;
 
 @Entity
 @Table(name = "owners")
@@ -33,61 +27,4 @@ public class Owner extends Person{
     @Pattern(regexp = "(^[0-9]{10})")
     private String telephone;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner", fetch = FetchType.EAGER)
-    private Set<Pet> pets;
-
-    @JsonIgnore
-    protected Set<Pet> getPetsInternal() {
-        if (this.pets == null) {
-            this.pets = new HashSet<>();
-        }
-        return this.pets;
-    }
-
-    protected void setPetsInternal(Set<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public List<Pet> getPets() {
-        List<Pet> sortedPets = new ArrayList<>(getPetsInternal());
-        PropertyComparator.sort(sortedPets, new MutableSortDefinition("name", true, true));
-        return Collections.unmodifiableList(sortedPets);
-    }
-
-    public void addPet(Pet pet) {
-        getPetsInternal().add(pet);
-        pet.setOwner(this);
-    }
-
-    public Pet getPet(String name) {
-        return getPet(name, false);
-    }
-
-    public Pet getPet(String name, boolean ignoreNew) {
-        name = name.toLowerCase();
-        for (Pet pet : getPetsInternal()) {
-            if (!ignoreNew || !pet.isNew()) {
-                String compName = pet.getName();
-                compName = compName.toLowerCase();
-                if (compName.equals(name)) {
-                    return pet;
-                }
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringCreator(this)
-
-                .append("id", this.getId())
-                .append("new", this.isNew())
-                .append("lastName", this.getLastName())
-                .append("firstName", this.getFirstName())
-                .append("address", this.address)
-                .append("city", this.city)
-                .append("telephone", this.telephone)
-                .toString();
-    }
 }
